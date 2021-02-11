ae288fbbc6074032b0055278d2128d9e7054ad6c
RubenVG <0964326@hr.nl>
2021-02-11 15:49:42 +0100

    Added issue + timer V1

---
+++ Src/Playground.Module/BusinessObjects/Issues/Issue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+[DefaultClassOptions]
+[ImageName("BO_Contact")]
+[DefaultProperty("DefaultProperty")]
+[CreatableItem(false)]
+public class Issue : BaseObject {
+    public Issue(Session session) : base(session) {
+    }
+
+
+    private string _title;
+
+    public string Title {
+        get => _title;
+        set => SetPropertyValue(nameof(Title), ref _title, value);
+    }
+
+    [ImmediatePostData]
+    public TimeSpan TotalTime => GetTime();
+
+
+
+    [DevExpress.Xpo.Aggregated]
+    [ExpandObjectMembers(ExpandObjectMembers.Never)]
+    [Association("Issue-Timers")]
+
+    public XPCollection<Timer> Timers
+    {
+        get => GetCollection<Timer>(nameof(Timers));
+    }
+
+
+
+
+    [VisibleInListView(false)]
+    [VisibleInDetailView(false)]
+    public string DefaultProperty => "Default Property";
+
+    public override void AfterConstruction() {
+        base.AfterConstruction();
+    }
+
+    private TimeSpan GetTime() {
+        TimeSpan EndTime;
+        EndTime = TimeSpan.Zero;
+        var ios = XPObjectSpace.FindObjectSpaceByObject(this);
+        var timers = ios.GetObjects<Timer>().Where(w => w.Issue == this);
+        foreach (var item in timers) {
+            EndTime = EndTime + item.TimeSpent;
+        }
+
+        return EndTime;
+    }
+}
---
+++ Src/Playground.Module/BusinessObjects/Issues/Timer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using DevExpress.CodeParser;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using DevExpress.XtraRichEdit.Commands;
+
+[DefaultClassOptions]
+[ImageName("BO_Contact")]
+[DefaultProperty("DefaultProperty")]
+[CreatableItem(false)]
+public class Timer : BaseObject {
+    public Timer(Session session) : base(session) {
+    }
+
+    private TimeSpan _startTimer;
+    [EditorAlias("dxTimeEditor")]
+    [ModelDefault("AllowEdit", "True")]
+    public TimeSpan StartTimer
+    {
+        get => _startTimer;
+        set => SetPropertyValue(nameof(StartTimer), ref _startTimer, value);
+    }
+
+    private string _work;
+    private Issue _issue;
+
+    [Size(FieldSizeAttribute.Unlimited)]
+    [ModelDefault("RowCount", "5")]
+    [ModelDefault("Size", "8192")]
+
+    public string Work {
+        get => _work;
+        set => SetPropertyValue(nameof(Work), ref _work, value);
+    }
+
+    [Association("Issue-Timers")]
+
+    public Issue Issue
+    {
+        get => _issue;
+        set => SetPropertyValue(nameof(Issue), ref _issue, value);
+    }
+
+    [VisibleInListView(false)]
+    [VisibleInDetailView(false)]
+
+    public TimeSpan TimeSpent => StartTimer == TimeSpan.Zero ? DateTime.Now.TimeOfDay : DateTime.Now.TimeOfDay - StartTimer;
+
+    [VisibleInListView(false)]
+    [VisibleInDetailView(false)]
+    public string DefaultProperty => StartTimer == TimeSpan.Zero ? "00:00" : TimeSpent.ToString();
+
+    public override void AfterConstruction() {
+        base.AfterConstruction();
+    }
+}
